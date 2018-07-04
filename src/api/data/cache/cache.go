6a7c2d78dcc4791faf77d22425b4dd109a0c33c6
Kun Peng Fan <fkpwolf@gmail.com>
2018-07-04 16:39:04 +0800

    make foreground repository refresh as goroutine
    Change-Id: I743d1b09eeba4e7b7c8c20aa0609722297c1c0ad

--- src/api/data/cache/cache.go
+++ src/api/data/cache/cache.go
@@ -234,11 +234,13 @@ func (c *cachedCharts) Refresh() error {
 	db, closer := c.dbSession.DB()
 	defer closer()
 
+	log.Info("Connected to db")
 	repos, err := models.ListRepos(db)
 	if err != nil {
 		return err
 	}
 	for _, repo := range repos {
+		log.Info("Indexing repo " + repo.Name)
 		charts, err := repohelper.GetChartsFromRepoIndexFile(repo)
 		if err != nil {
 			log.WithFields(log.Fields{
@@ -256,6 +258,7 @@ func (c *cachedCharts) Refresh() error {
 
 		// 3.1 - parallellize processing
 		for _, chart := range charts {
+			log.Info("Indexing chart " + *chart.Name)
 			go processChartMetadata(chart, repo.URL, ch)
 		}
 		// 3.2 Channel drain
@@ -273,6 +276,7 @@ func (c *cachedCharts) Refresh() error {
 	c.rwm.Lock()
 	c.allCharts = updatedCharts
 	c.rwm.Unlock()
+	log.Info("Refresh finished")
 	return nil
 }
 
@@ -300,6 +304,7 @@ func processChartMetadata(chart *swaggermodels.ChartPackage, repoURL string, out
 	if err != nil {
 		it.err = err
 		out <- it
+		log.Info("processChartMetadata return. Exist.")
 		return
 	}
 
@@ -328,5 +333,6 @@ func processChartMetadata(chart *swaggermodels.ChartPackage, repoURL string, out
 			}).Error("Error on DownloadAndProcessChartIcon")
 		}
 	}
+	log.Info("processChartMetadata return. Downloaded.")
 	out <- it
 }
